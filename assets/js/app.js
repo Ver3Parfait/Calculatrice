eea5d621de2d59e5d1374635f20c29ab3b315054
Ver3Parfait <militellolucas@icloud.com>
2022-12-24 01:03:58 +0100

    1

---
+++ assets/js/app.js
@@ -0,0 +1,372 @@
+let board = document.querySelector("#board");
+let cell = document.querySelectorAll(".cell").value;
+let result = document.querySelector("#result");
+let turn = 0;
+let font = 3;
+let resultshow = 0;
+let resultshow2 = 0;
+let resultReal = 0;
+let calc = "0";
+let calc2 = "0";
+let calcPars = 0;
+let calcPars2 = 0;
+let valueElem;
+let sign = "";
+let sign2 = "";
+let reset = document.querySelector('#AC')
+let onclick = document.querySelector('#test');
+let orange = 'background-color: orange;'
+
+function Calculator(elem) {
+console.log(calc2.length%9);
+
+  valueElem = elem.value;
+  if (valueElem == "add") {
+    Addition();
+    onclick.removeAttribute('id', 'color')
+    onclick = elem
+    onclick.setAttribute('id', 'color')
+  } else if (valueElem == "=") {
+    Equal();
+  } else if (valueElem == "+/-") {
+  } else if (valueElem == "substarct") {
+    onclick.removeAttribute('id', 'color')
+    onclick = elem
+    onclick.setAttribute('id', 'color')
+    Substraction();
+  } else if (valueElem == "x") {
+    onclick.removeAttribute('id', 'color')
+    onclick = elem
+    onclick.setAttribute('id', 'color')
+    Multiplication();
+  } else if (valueElem == "÷") {
+    onclick.removeAttribute('id', 'color')
+    onclick = elem
+    onclick.setAttribute('id', 'color')
+    Division();
+  } else if (valueElem == "AC") {
+    Erase();
+  } else if (valueElem == "C") {
+    EraseCalc();
+  } else {
+        onclick.removeAttribute('id', 'color')
+        TakeNumbers();
+        if (calc != "0" || calc2 != "0") {
+            reset.setAttribute('value', 'C')
+            reset.innerHTML = 'C'
+        }else{
+            reset.setAttribute('value', 'AC')
+            reset.innerHTML = 'AC'
+        }
+  }
+}
+
+function TakeNumbers() {
+  if (
+    valueElem == "AC" ||
+    valueElem == "+/-" ||
+    valueElem == "%" ||
+    valueElem == "÷" ||
+    valueElem == "x" ||
+    valueElem == "substarct" ||
+    valueElem == "add" ||
+    valueElem == "="
+  ) {
+    turn++;
+  } else {
+    if (turn % 2) {
+        if (sign == "") {
+            resultReal = 0
+            calc = calc + valueElem;
+            result.innerHTML = calc;
+        }else{
+            calc = calc + valueElem;
+            result.innerHTML = calc;
+        }
+    } else {
+        if (sign == "") {
+            resultReal = 0
+            calc2 = calc2 + valueElem;
+            result.innerHTML = calc2;
+        }else{
+      calc2 = calc2 + valueElem;
+      result.innerHTML = calc2;
+        }
+    }
+  }
+  calcPars = parseFloat(calc);
+  calcPars2 = parseFloat(calc2);
+}
+
+function Addition() {
+  if (turn % 2) {
+    if (sign == "substarct") {
+      Substraction();
+    } else if (sign == "+/-") {
+    } else if (sign == "x") {
+      Multiplication();
+    } else if (sign == "÷") {
+      Division();
+    } else {
+      resultshow = calcPars2 + calcPars;
+      resultReal = resultReal + resultshow;
+    }
+  } else {
+    if (sign == "substarct") {
+      Substraction();
+    } else if (sign == "+/-") {
+    } else if (sign == "x") {
+      Multiplication();
+    } else if (sign == "÷") {
+      Division();
+    } else {
+      resultshow2 = calcPars2 + calcPars;
+      resultReal = resultReal + resultshow2;
+    }
+  }
+  calcPars = 0;
+  calcPars2 = 0;
+  calc = "0";
+  resultshow = 0;
+  calc2 = "0";
+  resultshow2 = 0;
+  result.innerHTML = parseFloat(resultReal);
+  sign = valueElem;
+}
+
+function Substraction() {
+  if (turn % 2) {
+    if (resultReal == 0) {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "x") {
+        Multiplication();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultshow = calcPars2 + calcPars;
+        resultReal = resultshow - resultReal;
+      }
+    } else {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "x") {
+        Multiplication();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultshow = calcPars2 + calcPars;
+        resultReal = resultReal - resultshow;
+      }
+    }
+  } else {
+    if (resultReal == 0) {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "x") {
+        Multiplication();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultshow2 = calcPars2 + calcPars;
+        resultReal = resultshow2 - resultReal;
+      }
+    } else {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "x") {
+        Multiplication();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultshow2 = calcPars2 + calcPars;
+        resultReal = resultReal - resultshow2;
+      }
+    }
+  }
+  calcPars = 0;
+  calcPars2 = 0;
+  calc = "0";
+  calc2 = "0";
+  resultshow = 0;
+  resultshow2 = 0;
+  result.innerHTML = parseFloat(resultReal);
+  sign = valueElem;
+}
+
+function Multiplication() {
+  if (turn % 2) {
+    if (resultReal == 0) {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultReal = calcPars;
+      }
+    } else if (sign == "") {
+        calcPars = resultReal
+    } else {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultReal = resultReal * calcPars;
+      }
+    }
+  } else {
+    if (resultReal == 0) {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultReal = calcPars2;
+      }
+    } else if (sign == "") {
+        calcPars2 = resultReal
+    } else {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "÷") {
+        Division();
+      } else {
+        resultReal = resultReal * calcPars2;
+      }
+    }
+  }
+  calcPars = 0;
+  calcPars2 = 0;
+  calc2 = "0";
+  resultshow2 = 0;
+  calc = "0";
+  resultshow = 0;
+  result.innerHTML = parseFloat(resultReal);
+  sign = valueElem;
+}
+
+function Division() {
+  if (turn % 2) {
+    if (resultReal == 0) {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "x") {
+        Multiplication();
+      }else{
+        resultReal = calcPars;
+      }
+    } else if (sign == "") {
+        calcPars = resultReal
+    } else {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "x") {
+        Multiplication();
+      }else{
+      resultReal = resultReal / calcPars;
+      }
+    }
+  } else {
+    if (resultReal == 0) {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "x") {
+        Multiplication();
+      }else{
+        resultReal = calcPars2;
+      }
+    } else if (sign == "") {
+        calcPars2 = resultReal
+    } else {
+      if (sign == "add") {
+        Addition();
+      } else if (sign == "+/-") {
+      } else if (sign == "substarct") {
+        Substraction();
+      } else if (sign == "x") {
+        Multiplication();
+      }else{
+        resultReal = resultReal / calcPars2;
+      }
+    }
+  }
+  calcPars = 0;
+  calcPars2 = 0;
+  calc2 = "0";
+  resultshow2 = 0;
+  calc = "0";
+  resultshow = 0;
+  result.innerHTML = parseFloat(resultReal);
+  sign = valueElem;
+}
+
+function Equal() {
+  if (sign == "add") {
+    resultReal = resultReal + (calcPars + calcPars2);
+  } else if (sign == "substarct") {
+    resultReal = resultReal - (calcPars + calcPars2);
+  } else if (sign == "x") {
+    resultReal = resultReal * (calcPars + calcPars2);
+  } else if (sign == "÷") {
+    resultReal = resultReal / (calcPars + calcPars2);
+  }
+  sign = "";
+  calcPars = 0;
+  calcPars2 = 0;
+  calc = "0";
+  calc2 = "0";
+  resultshow = 0;
+  resultshow2 = 0;
+  result.innerHTML = parseFloat(resultReal);
+}
+
+function Erase() {
+    onclick.removeAttribute('id', 'color')
+    sign = "",
+  calcPars = 0;
+  calcPars2 = 0;
+  calc = "0";
+  calc2 = "0";
+  resultshow = 0;
+  resultshow2 = 0;
+  resultReal = 0;
+  result.innerHTML = "";
+}
+
+function EraseCalc() {
+  calcPars = 0;
+  calcPars2 = 0;
+  calc = "0";
+  calc2 = "0";
+  resultshow = 0;
+  resultshow2 = 0;
+  result.innerHTML = "";
+  reset.setAttribute('value', 'AC')
+  reset.innerHTML = 'AC'
+}
